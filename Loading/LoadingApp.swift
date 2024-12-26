e79828d527166ef2e9c0dc73417c4992946f7ba9
Hessam Mahdiabadi <h.mahdiabadi@bluteam.ir>
2024-12-26 12:43:04 +0330

    Initial Commit

---
+++ Loading/LoadingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingApp.swift
+//  Loading
+//
+//  Created by HEssam on 12/26/24.
+//
+
+import SwiftUI
+
+@main
+struct LoadingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
